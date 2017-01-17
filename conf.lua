b8440df49f8fa8613fb541b5ed978e9fc86e8d58
Manuel Xarez <manuelxarez@ua.pt>
2017-01-17 15:26:31 +0000

    Jump fixed?

--- conf.lua
+++ conf.lua
@@ -1,7 +1,6 @@
 -- Configuration
 function love.conf(t)
 	t.title = "Reaper Inc"
-	t.version = "0.01"
 	t.width = 800
 	t.height = 480
 
--- main.lua
+++ main.lua
@@ -9,18 +9,26 @@ function love.load()
 	reaper = {}
 	reaper.img = love.graphics.newImage('assets/imgs/reaper.jpg')
 	reaper.body = love.physics.newBody(world, 0, love.graphics.getHeight() - reaper.img:getHeight(), "dynamic")
-	reaper.body:setMass(reaper.img:getWidth()/2, reaper.img:getHeight()/2, 70, 1)
+	reaper.body:setMass(70)
 	reaper.shape = love.physics.newRectangleShape(200,200)
 	reaper.fixture = love.physics.newFixture(reaper.body, reaper.shape)
 	reaper.fixture:setRestitution(0.1)
 	reaper.fixture:setUserData("Reaper")
 
 	-- Platform
-	platform = {}
-	platform.body = love.physics.newBody(world, 0, love.graphics.getHeight(), "static")
-	platform.shape = love.physics.newRectangleShape(800,50)
-	platform.fixture = love.physics.newFixture(platform.body, platform.shape)
-	platform.fixture:setUserData("Platform")
+	p1 = {}
+	p2 = {}
+	platforms = { }
+	p1.body = love.physics.newBody(world, 0, love.graphics.getHeight(), "static")
+	p1.shape = love.physics.newRectangleShape(800,50)
+	p1.fixture = love.physics.newFixture(p1.body, p1.shape)
+	p1.fixture:setUserData("Platform1")
+	p2.body = love.physics.newBody(world, 400, love.graphics.getHeight(), "static")
+	p2.shape = love.physics.newRectangleShape(50,120)
+	p2.fixture = love.physics.newFixture(p2.body, p2.shape)
+	p2.fixture:setUserData("Platform2")
+	platforms[0] = p1
+	platforms[1] = p2
 end
 
 -- Called on every frame
@@ -38,15 +46,14 @@ function love.update(dt)
 end
 
 function love.keypressed(key)
-	if key == "space" then
-		if next(reaper.body:getContactList()) ~= nil then
-			reaper.body:applyLinearImpulse(0,30000)
-		end
+	if key == 'space' and next(reaper.body:getContactList()) ~= nil then
+		reaper.body:applyLinearImpulse(0,30000)
 	end
 end
 
 -- Called on every frame
 function love.draw()
-	love.graphics.polygon("fill", platform.body:getWorldPoints(platform.shape:getPoints()))
+	love.graphics.polygon("fill", platforms[0].body:getWorldPoints(platforms[0].shape:getPoints()))
+	love.graphics.polygon("fill", platforms[1].body:getWorldPoints(platforms[1].shape:getPoints()))
 	love.graphics.draw(reaper.img, reaper.body:getX(), reaper.body:getY(), reaper.body:getAngle(),  1, 1, reaper.img:getWidth()/2, reaper.img:getHeight()/2)
 end
